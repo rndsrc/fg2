4e2e1dbef3d2a007e6f81ff7c06658253a70830f
Chi-kwan Chan <ckch@nordita.org>
2011-11-11 11:38:50 +0100

    A minimal executable compiled by nvcc

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/* Copyright (C) 2011 Chi-kwan Chan
+   Copyright (C) 2011 NORDITA
+
+   This file is part of fg2.
+
+   Fg2 is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Fg2 is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with fg2.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <cstdio>
+#include "fg2.h"
+
+int main(int argc, char **argv)
+{
+  printf("2D finite grid code written in CUDA C\n");
+
+  return 0;
+}
