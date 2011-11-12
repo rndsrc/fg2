65a0dfea6154f9ed4ec0e43f14ed3912d100846c
Chi-kwan Chan <ckch@nordita.org>
2011-11-12 01:59:05 +0100

    Report global and shared memory usages

--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,8 @@ int main(int argc, char **argv)
 {
   const char *input = "default";
 
-  Z n0 = 10, n1 = 1024, n2 = 1024;
-  R t  = 1.;
-
-  int d = 0;
+  Z d = 0, n0 = 10, n1 = 1024, n2 = 1024;
+  R t = 1;
 
   // If "--help" is an argument, print usage and exit
   for(int i = 1; i < argc; ++i)
@@ -54,30 +52,39 @@ int main(int argc, char **argv)
   }
   print("2D finite grid code written in CUDA C\n\n");
 
-  // Pick a device
+  // Pick a device, obtain global and shared memory size
+  double gsz = 0.0, ssz = 0.0;
   int count; cudaGetDeviceCount(&count);
-  print("  Device %d/%d : ", d, count);
+  print("  Device %d/%d   : ", d, count);
   if(d < count) {
     if(cudaSuccess == cudaSetDevice(d)) {
       cudaDeviceProp dev; cudaGetDeviceProperties(&dev, d);
-      print("\"%s\" with %gMiB of memory\n",
-            dev.name, dev.totalGlobalMem / 1048576.0);
+      gsz = dev.totalGlobalMem;
+      ssz = dev.sharedMemPerBlock;
+      print("\"%s\" with %gMiB global and %gKiB shared memory\n",
+            dev.name, gsz / 1048576.0, ssz / 1024.0);
     } else
       error("fail to pick device, QUIT\n");
   } else
     error("does not exist, QUIT\n");
 
   // Setup the grid and global variables
-  print("  Resolution : %d x %d", n1, n2);
+  print("  Resolution   : %d x %d", n1, n2);
   if(int sz = setup(n1, n2))
-    print(" using %gMiB of memory\n", sz / 1048576.0);
+    print(" using %.3gMiB (%.3g%%) of global memory\n",
+          sz / 1048576.0, 100.0 * sz / gsz);
   else
     error(", fail to allocate memory, QUIT\n");
 
+  print("  Grid x block : (%d x %d) x (%d x %d)",
+        global::g1, global::g2, global::b1, global::b2);
+  print(" using %.3gKiB (%.3g%%) of shared memory\n",
+        global::sz / 1024.0, 100.0 * global::sz / ssz);
+
   // TODO: setup initial condition or load starting frame from input
-  print("  Initialize : \"%s\"\n", input);
+  print("  Initialize   : \"%s\"\n", input);
 
   // Really solve the problem
-  print("  Time       : %g with %d frame%s\n", t, n0, n0 > 1 ? "s" : "");
+  print("  Time         : %g with %d frame%s\n", t, n0, n0 > 1 ? "s" : "");
   return solve(0.0, t, 0, n0);
 }
