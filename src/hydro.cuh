8221e7e1033dcf31e6566b3d7117b924898ab50d
Chi-kwan Chan <ckch@nordita.org>
2011-11-14 19:03:45 +0100

    Pick the correct value of thermal conductivity
    For ideal gas, K = cp mu.  Therefore, kappa = gamma nu / (gamma - 1).

--- src/hydro.cuh
+++ src/hydro.cuh
@@ -17,9 +17,9 @@
    along with fg2.  If not, see <http://www.gnu.org/licenses/>. */
 
 __device__ __constant__ R para_gamma = 5.0 / 3.0;
-__device__ __constant__ R para_nus   = 5e-4; // shear viscosity
-__device__ __constant__ R para_nub   = 0.0;  // bulk  viscosity
-__device__ __constant__ R para_kappa = 5e-4; // thermal conductivity
+__device__ __constant__ R para_nus   = 5.0e-4;  // shear viscosity
+__device__ __constant__ R para_nub   = 0.0;     // bulk  viscosity
+__device__ __constant__ R para_kappa = 1.25e-3; // thermal conductivity
 
 static __device__ S eqns(const S *u, const R d1, const R d2, const Z s)
 {
