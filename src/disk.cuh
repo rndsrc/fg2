b72b5de318b3f4ef3ba53046bd6840af8231f412
Chi-kwan Chan <ckch@nordita.org>
2012-02-02 19:02:32 +0100

    Employ "specific accretion rate" a = ur * r^2

--- src/disk.cuh
+++ src/disk.cuh
@@ -19,8 +19,8 @@
 #define PARA_R0 K(3.0)
 
 struct state {
-  R ld;        // ln(density)
-  R ur, ut, l; // u_r, u_theta, specific angular momentum
+  R ld;      // ln(density)
+  R a, v, l; // specific accretion rate, u_theta, specific angular momentum
 };
 
 #ifdef KICK_CU ///////////////////////////////////////////////////////////////
@@ -33,23 +33,25 @@ static __device__ S eqns(const S *u, const Z i, const Z j, const Z s)
   S dt = {0.0, 0.0, 0.0, 0.0};
 
   const R r     = PARA_R0 * exp((i + K(0.5)) * Delta1); // 4 FLOP
+  const R r2    = r * r;                                // 1 FLOP
   const R theta =               (j + K(0.5)) * Delta2 ; // 2 FLOP
-  const R cot_t = cos(theta) / sin(theta);              // 3 FLOP
+  const R sin_t = sin(theta);                           // 1 FLOP
+  const R cot_t = cos(theta) / sin_t;                   // 2 FLOP
 
-  const R ur    = u->ur;
-  const R ut    = u->ut;
-  const R uphi  = u->l / (r * sin(theta)); // 3 FLOP
+  const R ur    = u->a / r2;          // 1 FLOP
+  const R utheta= u->v;
+  const R uphi  = u->l / (r * sin_t); // 2 FLOP
 
-  const S d1    = {D1(ld), D1(ur), D1(ut), D1(l)};                // 36 FLOP
-  const S d2    = {D2(ld), D2(ur), D2(ut), D2(l)};                // 36 FLOP
-  const R div_u = (d1.ur + d2.ut + K(2.0) * ur + ut * cot_t) / r; //  6 FLOP
+  const S d1    = {D1(ld), D1(a), D1(v), D1(l)};           // 36 FLOP
+  const S d2    = {D2(ld), D2(a), D2(v), D2(l)};           // 36 FLOP
+  const R div_u = (d1.a / r2 + d2.v + utheta * cot_t) / r; //  5 FLOP
 
   // Advection: 20 FLOP
   {
-    dt.ld -= (ur * d1.ld + ut * d2.ld) / r;
-    dt.ur -= (ur * d1.ur + ut * d2.ur) / r;
-    dt.ut -= (ur * d1.ut + ut * d2.ut) / r;
-    dt.l  -= (ur * d1.l  + ut * d2.l ) / r;
+    dt.ld -= (ur * d1.ld + utheta * d2.ld) / r;
+    dt.a  -= (ur * d1.a  + utheta * d2.a ) / r;
+    dt.v  -= (ur * d1.v  + utheta * d2.v ) / r;
+    dt.l  -= (ur * d1.l  + utheta * d2.l ) / r;
   }
 
   // Compressible/pressure effects: 1 FLOP
@@ -57,17 +59,17 @@ static __device__ S eqns(const S *u, const Z i, const Z j, const Z s)
     dt.ld -= div_u;
   }
 
-  // Pseudo force (coordinate effect): 10 FLOP
+  // Pseudo force (coordinate effect): 13 FLOP
   {
     const R uphi2 = uphi * uphi;
-    dt.ur += (uphi2         + ut * ut) / r;
-    dt.ut += (uphi2 * cot_t - ur * ut) / r;
+    dt.a += (K(2.0) * ur * ur + uphi2 + utheta * utheta) * r;
+    dt.v += (           cot_t * uphi2 -     ur * utheta) / r;
   }
 
-  // External force (gravity)
+  // External force (gravity): 5 FLOP
   {
     const R r_rS = r - para_rS;
-    dt.ur -= para_M / (r_rS * r_rS);
+    dt.a -= r2 * para_M / (r_rS * r_rS);
   }
 
   return dt;
@@ -92,7 +94,7 @@ static void config(void)
   // Compute floating point operation and bandwidth per step
   const Z m1 = n1 + ORDER;
   const Z m2 = n2 + ORDER;
-  flops = 3 * ((n1 * n2) * (121 + NVAR * 2.0)); // assume FMA
+  flops = 3 * ((n1 * n2) * (129 + NVAR * 2.0)); // assume FMA
   bps   = 3 * ((m1 * m2) * 1.0 +
                (n1 * n2) * 5.0 +
                (m1 + m2) * 2.0 * ORDER) * NVAR * sizeof(R) * 8;
